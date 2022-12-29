cbda0ed1a1e0e1245044900f7b084d504778667d
Aysinghal <aysinghal06@gmail.com>
2022-12-29 01:54:35 -0500

    Clean Up Project
    Remove unnecessary files and re organize files

--- test.py
+++ test.py
@@ -1,31 +1,38 @@
-coordinate = input("Enter your coordinate (x,y): ")
-coordinate = coordinate.split(",")
-x_coordinate = int(coordinate[0][1:])
-y_coordinate = int(coordinate[1][:-1])
+import random
+
+def derivative(x_vals, y_vals, slope, intercept, learning_rate):
+    step_size_intercept = 0
+    step_size_slope = 0
+    for i in range(0, len(x_vals)):
+        step_size_intercept += (-2 * (y_vals[i] - (intercept + (slope * x_vals[i])))) * learning_rate
+        step_size_slope += (-2 * x_vals[i] * (y_vals[i] - (intercept + (slope * x_vals[i])))) * learning_rate
+    intercept -= step_size_intercept
+    slope -= step_size_slope
+
+    return [intercept, slope]
 
 
-def find_closest_ten(value):
-    value = abs(value)
-    test_val = 0
-    while True:
-        test_val += 10
-        if 10 >= test_val - value > 0:
-            return test_val
-            break
+reps = int(input("Number of repetitions (the more reps, the more accurate the result): "))
+learning_rate = float(input("Learning rate (the lower the rate, the more accurate the result): "))
 
+x_vals = input("List all of the X values (separate by comma): ")
+y_vals = input("List all of the Y values (separate by comma): ")
+x_vals = x_vals.split(",")
+y_vals = y_vals.split(",")
+map_x_vals = map(int, x_vals)
+map_y_vals = map(int, y_vals)
+x_vals = list(map_x_vals)
+y_vals = list(map_y_vals)
+#x_vals = [0, 1, 2, 3, 4, 5, 6]
+#y_vals = [4,7,10,13,16,19,22]
 
-for i in range(-1 * find_closest_ten(y_coordinate), find_closest_ten(y_coordinate)):
-    row = ''
-    if i == 0:
-        for j in range(-1 * find_closest_ten(x_coordinate), find_closest_ten(x_coordinate)):
-            row += "- "
-        print(row)
-        continue
-    for j in range(-1 * find_closest_ten(x_coordinate), find_closest_ten(x_coordinate)):
-        if j == 0:
-            row += '| '
-        elif j == x_coordinate and i == y_coordinate:
-            row += "* "
-        else:
-            row += '  '
-    print(row)
+#learning_rate = 0.01
+intercept = random.random()
+slope = random.random()
+
+for i in range(0, reps):
+    output = derivative(x_vals, y_vals, slope, intercept, learning_rate)
+    intercept = output[0]
+    slope = output[1]
+
+print(str(slope) + "x + " + str(intercept))
