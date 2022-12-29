c92206efd3049d0edaf4d587f2307790b617d84a
Aysinghal <aysinghal06@gmail.com>
2022-12-29 01:51:41 -0500

    Update Quadratic_Regression.py
    Fixed c learning issue, learning rate was too high

--- Quadratic_Regression.py
+++ Quadratic_Regression.py
@@ -1,22 +1,26 @@
 import matplotlib.pyplot as plt
+import time
 
 
-def quadratic_regression(x_data, y_data, learning=0.001, reps=1000000):
-    a = 0
-    b = 0
-    c = 0
+def quadratic_regression(x_data, y_data, learning=0.00000000001, reps=100000):
+    a = float(0)
+    b = float(0)
+    c = float(0)
 
-    for i in range(0, reps):
+    for i in range(1, reps + 1):
         a, b, c = step_size(x_data, y_data, learning, a, b, c)
+        #print(i, "/", reps)
+        #print("<" + ("-" * int(i / reps * 15)).ljust(15, ' ') + ">")
+        #print("\n" * 2)
 
     return [round(a, 8), round(b, 8), round(c, 8)]
     # return [round(a, 4), round(b, 4), round(c, 4)]
 
 
 def step_size(x_data, y_data, learning, a, b, c):
-    step_size_a = 0
-    step_size_b = 0
-    step_size_c = 0
+    step_size_a = float(0)
+    step_size_b = float(0)
+    step_size_c = float(0)
 
     for i in range(0, len(x_data)):
         step_size_a += (-2 * x_data[i] ** 2) * (y_data[i] - (a * x_data[i] ** 2) - (b * x_data[i]) - c)
@@ -25,41 +29,57 @@ def step_size(x_data, y_data, learning, a, b, c):
 
     step_size_a *= learning
     step_size_b *= learning
-    step_size_c *= learning
+    step_size_c *= (learning * 100000)
 
     a -= step_size_a
     b -= step_size_b
     c -= step_size_c
+    print(c)
 
     return a, b, c
 
 
-x_data = [-3, -2, -1, 0, 1, 2, 3]
-y_data = [18, 8, 2, 0, 2, 8.7, 18]
+def data_regression(x_data, y_data, data_type):
+    print(data_type + " - Regression Starting")
+    time.sleep(2)
 
-values = quadratic_regression(x_data, y_data)
+    values = quadratic_regression(x_data, y_data)
 
-print(values)
-x = -1 * (values[1] / (2 * values[0]))
-print("Vertex: (" + str(x) + ", " + str((values[0] * x ** 2) + (values[1] * x) + values[2]) + ")")
+    print(data_type + " - Regression Complete")
+    print(values)
 
-'''error = 0
-for i in range(0, len(x_data)):
-    error += (y_data[i] - ((values[0] * (x_data[i] ** 2)) + (values[1] * x_data[i]) + values[2])) ** 2
-print(error)'''
+    x = -1 * (values[1] / (2 * values[0]))
+    print("Vertex: (" + str(x) + ", " + str((values[0] * x ** 2) + (values[1] * x) + values[2]) + ")")
+    print("\n\n")
+    time.sleep(2)
 
-a = []
-b = []
+    '''error = 0
+    for i in range(0, len(x_data)):
+        error += (y_data[i] - ((values[0] * (x_data[i] ** 2)) + (values[1] * x_data[i]) + values[2])) ** 2
+    print(error)'''
 
-x = min(x_data)
+    a = []
+    b = []
 
-while x <= max(x_data) + 0.1:
-    y = (values[0] * x ** 2) + (values[1] * x) + values[2]
-    a.append(x)
-    b.append(y)
-    x += 0.1
+    x = min(x_data)
+    while x <= max(x_data) + 0.1:
+        y = (values[0] * x ** 2) + (values[1] * x) + values[2]
+        a.append(x)
+        b.append(y)
+        x += 0.1
 
-plt.scatter(x_data, y_data, color='green')
-plt.plot(a, b, color="red")
+    plt.scatter(x_data, y_data, label=data_type + " - raw")
+    plt.plot(a, b, label=data_type + " - learned")
 
+
+y_data = []
+x_data = []
+for i in range(-200, 200, 2):
+    y_data.append(i ** 2 + 5 * i + 300)
+    x_data.append(i)
+
+
+data_regression(x_data, y_data, "Test")
+
+plt.legend()
 plt.show()
